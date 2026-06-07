fa5ead2807c7866f6ac6d5eec72d21c54e49960b
dforfolapo <you@example.com>
2026-06-07 23:34:44 +0100

    Add PWA mainfest and update app icons

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,6 +9,9 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
       includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
       manifest: {
         name: 'Vurdict',
