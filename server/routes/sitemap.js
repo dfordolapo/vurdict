838b8735c2f4504996eb97f308f2068ced116005
dforfolapo <dfordolapo@gmail.com>
2026-06-19 01:49:24 +0100

    Include all URL variants (www/non-www, http/https) in sitemap

--- server/routes/sitemap.js
+++ server/routes/sitemap.js
@@ -2,53 +2,32 @@ import { Router } from 'express';
 
 const router = Router();
 
+const pages = [
+  { path: '/', freq: 'daily', priority: '1.0' },
+  { path: '/analyze', freq: 'weekly', priority: '0.8' },
+  { path: '/framework', freq: 'weekly', priority: '0.8' },
+  { path: '/report-card', freq: 'weekly', priority: '0.8' },
+  { path: '/faq', freq: 'monthly', priority: '0.6' },
+  { path: '/privacy', freq: 'monthly', priority: '0.3' },
+  { path: '/terms', freq: 'monthly', priority: '0.3' },
+  { path: '/support', freq: 'monthly', priority: '0.3' },
+  { path: '/revurdict', freq: 'weekly', priority: '0.5' },
+];
+
+const origins = [
+  'https://vurdict.site',
+  'https://www.vurdict.site',
+  'http://vurdict.site',
+  'http://www.vurdict.site',
+];
+
+const urls = origins.flatMap(origin =>
+  pages.map(p => `  <url>\n    <loc>${origin}${p.path}</loc>\n    <changefreq>${p.freq}</changefreq>\n    <priority>${p.priority}</priority>\n  </url>`)
+);
+
 const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>https://www.vurdict.site/</loc>
-    <changefreq>daily</changefreq>
-    <priority>1.0</priority>
-  </url>
-  <url>
-    <loc>https://www.vurdict.site/analyze</loc>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>https://www.vurdict.site/framework</loc>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>https://www.vurdict.site/report-card</loc>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>https://www.vurdict.site/faq</loc>
-    <changefreq>monthly</changefreq>
-    <priority>0.6</priority>
-  </url>
-  <url>
-    <loc>https://www.vurdict.site/privacy</loc>
-    <changefreq>monthly</changefreq>
-    <priority>0.3</priority>
-  </url>
-  <url>
-    <loc>https://www.vurdict.site/terms</loc>
-    <changefreq>monthly</changefreq>
-    <priority>0.3</priority>
-  </url>
-  <url>
-    <loc>https://www.vurdict.site/support</loc>
-    <changefreq>monthly</changefreq>
-    <priority>0.3</priority>
-  </url>
-  <url>
-    <loc>https://www.vurdict.site/revurdict</loc>
-    <changefreq>weekly</changefreq>
-    <priority>0.5</priority>
-  </url>
+${urls.join('\n')}
 </urlset>`;
 
 router.get('/', (_req, res) => {
